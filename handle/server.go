10ebf77d9350ee69fbbbb637654bfc8c1bca7275
wyx2685 <wyx2685@gmail.com>
2024-09-21 22:34:22 +0900

    解决节点排序问题

--- handle/server.go
+++ handle/server.go
@@ -1,7 +1,9 @@
 package handle
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -10,17 +12,14 @@ import (
 )
 
 var servers map[string]*v2b.ServerInfo
+var orderservers []string
 var updateTime time.Time
 
 func GetServers(c *gin.Context) {
-	if len(servers) != 0 {
-		if time.Now().Before(updateTime) {
-			c.JSON(200, Rsp{
-				Success: true,
-				Data:    servers,
-			})
-			return
-		}
+	if len(servers) != 0 && time.Now().Before(updateTime) {
+		orderedJSON := buildOrderedJSON()
+		c.Data(200, "application/json", []byte(orderedJSON))
+		return
 	}
 	r, err := v2b.GetServers()
 	if err != nil {
@@ -29,19 +28,33 @@ func GetServers(c *gin.Context) {
 		return
 	}
 	updateTime = time.Now().Add(180 * time.Hour)
-	if len(r) != 0 {
-		servers = make(map[string]*v2b.ServerInfo, len(r))
-		for i := range r {
-			servers[fmt.Sprintf(
-				"%s_%d",
-				r[i].Type,
-				r[i].Id,
-			)] = &r[i]
+
+	servers = make(map[string]*v2b.ServerInfo, len(r))
+	orderservers = make([]string, 0, len(r))
+	for i := range r {
+		key := fmt.Sprintf("%s_%d", r[i].Type, r[i].Id)
+		servers[key] = &r[i]
+		orderservers = append(orderservers, key)
+	}
+	orderedJSON := buildOrderedJSON()
+	c.Data(200, "application/json", []byte(orderedJSON))
+}
+
+func buildOrderedJSON() string {
+	var sb strings.Builder
+
+	sb.WriteString(`{"success": true, "data": {`)
+	for i, key := range orderservers {
+		server := servers[key]
+		serverJSON, err := json.Marshal(server)
+		if err != nil {
+			continue
+		}
+		sb.WriteString(fmt.Sprintf(`"%s": %s`, key, serverJSON))
+		if i < len(orderservers)-1 {
+			sb.WriteString(",")
 		}
 	}
-	c.JSON(200, Rsp{
-		Success: true,
-		Data:    servers,
-	})
-	return
+	sb.WriteString("}}")
+	return sb.String()
 }
